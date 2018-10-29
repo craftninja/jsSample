ff4bba73136dba45d72f567b16406b40e3d78739
Emily Platzer <emilyplatzer@gmail.com>
2018-10-29 09:54:24 -0600

    Number of blocks needed to create a triangle can be calculated from num of rows in triangle

--- spec/triangleMadeOfBlocks.spec.js
+++ spec/triangleMadeOfBlocks.spec.js
@@ -7,4 +7,10 @@ describe('triangleMadeOfBlocks', () => {
   it('returns 1 total blocks for triangle with 1 row', () => {
     expect(triangleMadeOfBlocks(1)).toEqual(1);
   });
+  it('returns 3 total blocks for triangle with 2 rows', () => {
+    expect(triangleMadeOfBlocks(2)).toEqual(3);
+  });
+  it('returns 15 total blocks for triangle with 5 rows', () => {
+    expect(triangleMadeOfBlocks(5)).toEqual(15);
+  });
 });
