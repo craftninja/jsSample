e677f490eab5bcb42e29c87fc6cca02e4e413d6d
Emily Platzer <emilyplatzer@gmail.com>
2018-10-29 09:14:44 -0600

    Triangle with one row is made up of one block

--- spec/triangleMadeOfBlocks.spec.js
+++ spec/triangleMadeOfBlocks.spec.js
@@ -4,4 +4,7 @@ describe('triangleMadeOfBlocks', () => {
   it('returns 0 total blocks for triangle with 0 rows', () => {
     expect(triangleMadeOfBlocks(0)).toEqual(0);
   });
+  it('returns 1 total blocks for triangle with 1 row', () => {
+    expect(triangleMadeOfBlocks(1)).toEqual(1);
+  });
 });
