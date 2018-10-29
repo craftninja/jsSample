0c16522fe853ee01cd8416f557d89dba7fdc5cc4
Emily Platzer <emilyplatzer@gmail.com>
2018-10-29 09:04:56 -0600

    Bunny ears can be calculated for lots of bunnies

--- spec/bunnyEars.spec.js
+++ spec/bunnyEars.spec.js
@@ -4,4 +4,16 @@ describe ('bunnyEars', () => {
   it('returns 0 ears for 0 bunnies', () => {
     expect(bunnyEars(0)).toEqual(0);
   });
+  it('returns 2 ears for 1 bunnies', () => {
+    expect(bunnyEars(1)).toEqual(2);
+  });
+  it('returns 4 ears for 2 bunnies', () => {
+    expect(bunnyEars(2)).toEqual(4);
+  });
+  it('returns 6 ears for 3 bunnies', () => {
+    expect(bunnyEars(2)).toEqual(4);
+  });
+  it('returns 6 ears for 3 bunnies', () => {
+    expect(bunnyEars(2)).toEqual(4);
+  });
 });
